7dca371f4bcac6c7aa4c9cd8c12d5d6afb4a899c
YiNo <dean@airdb.com>
2019-06-05 12:38:10 +0800

    update osinit

--- vagrant/base-ubuntu/init.sh
+++ vagrant/base-ubuntu/init.sh
@@ -6,5 +6,4 @@ ln -snf /usr/share/zoneinfo/Asia/Singapore /etc/localtime && echo 'Asia/Singapor
 export RUNNING_CONTEXT=docker
 export INSTALL_PACKAGES=yes
 apt-get -y install python-pip python-dev libmysqlclient-dev
-pip install -U pip
 pip install MySQL-python
